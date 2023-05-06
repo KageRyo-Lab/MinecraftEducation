657ddb092fffe4204ef38eb6e4c43b83c53fce2c
WANG <32015407+Terrible-creeper@users.noreply.github.com>
2023-05-06 13:34:35 +0800

    Update 009-PalaeontologyPuzzle.py

---
+++ HOUR OF CODE 2021 - TIMECRAFT/009-PalaeontologyPuzzle.py
@@ -0,0 +1,14 @@
+# 在這裡編寫程式碼
+
+#修改前
+#agent.give("BONEBLOCK", 64, 1)
+#修改後
+#agent.give("BONE BLOCK", 64, 1)
+
+agent.give("BONE BLOCK", 64, 1)
+
+for i in range(4):
+    agent.move("forward")
+    agent.move("forward")
+    agent.destroy("down")
+    agent.place("down", 1)
