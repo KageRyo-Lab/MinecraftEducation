4093b56a53aeafcfbdc004ebc96549dfc6819fea
WANG <32015407+Terrible-creeper@users.noreply.github.com>
2023-05-06 13:31:29 +0800

    Update 008-Human'sBestFriend.py

--- HOUR OF CODE 2021 - TIMECRAFT/008-Human'sBestFriend.py
+++ HOUR OF CODE 2021 - TIMECRAFT/008-Human'sBestFriend.py
@@ -1,6 +1,12 @@
+#官方給予的程式碼有誤
+#修改前
+#agent.drop(1, 1) 
+#修改後
+#agent.drop("BEETROOT", 1) 
+
 agent.give("BEETROOT", 64, 1)
 for i in range(6):
-    agent.drop(1,1)
+    agent.drop("BEETROOT", 1) 
     agent.move("forward")
     agent.move("forward")
     agent.move("forward")
