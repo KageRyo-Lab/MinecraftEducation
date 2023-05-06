5a260e92e82d166d6a1139ffe1b891531c758829
WANG <32015407+Terrible-creeper@users.noreply.github.com>
2023-05-06 13:11:55 +0800

    Update 006-FirstFlights.py

---
+++ HOUR OF CODE 2021 - TIMECRAFT/006-FirstFlights.py
@@ -0,0 +1,24 @@
+# 在這裡編寫程式碼
+
+agent.give("DIRT", 64, 1)
+for i in range(2):
+    agent.move("forward")
+    agent.place("down", 1)
+
+agent.move("forward")
+agent.move("forward")
+agent.move("right")
+agent.move("right")
+
+for i in range(2):
+    agent.move("forward")
+    agent.place("down", 1)
+
+agent.move("forward")
+agent.move("forward")
+agent.move("right")
+agent.move("right")
+
+for i in range(2):
+    agent.move("forward")
+    agent.place("down", 1)
